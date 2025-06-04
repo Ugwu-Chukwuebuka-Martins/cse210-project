ed8674fb44492a4e3d0875517ac1fa96e91ae9d0
ugwu-chukwuebuka-martins <cugwu@byupathway.edu>
2025-06-04 15:29:38 -0400

    perfect

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.WriteLine("-----------------------------------------------------");
+        Assignment assign1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment mathAssign = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssign.GetSummary());
+        Console.WriteLine(mathAssign.GetHomeworkList());
+
+        WritingAssignment writeAssign = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writeAssign.GetSummary());
+        Console.WriteLine(writeAssign.GetWritingInformation());
+        Console.WriteLine("-----------------------------------------------------");
     }
 }
